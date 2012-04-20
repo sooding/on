bdd369baec40b267b9b3e8ce4fb951c7f769fc6b
Your Name <moneikmarie@gmail.com>
2012-04-20 15:03:49 -0500

    dsd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
    end
 
    def create
-	   render 'new'
+	   company = Company.find_by_name(params[:session][:name])
+	   if company && company.authenticate(params[:session][:password])
+		   #redirect_to 
+	   else 
+                   flash[:error] = 'Invalid email/password combination'
+		   render 'new'
+	   end
+		 
    end
 
    def destroy
